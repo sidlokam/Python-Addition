f435211e358fa55b5d6bfe09a10b89398ced9fa3
Siddharth Lokam <sidhu.lokam@gmail.com>
2016-12-29 14:29:05 -0500

    adding program

---
+++ Addition.py
@@ -0,0 +1,8 @@
+def addition(number1, number2) :
+    return number1 + number2
+
+number1 = int(input("Enter First Number : "))
+number2 = int(input("Enter Second Number : "))
+number3 = addition(number1, number2)
+print("the sum of the two numbers is : "+ str(number3)) 
+
